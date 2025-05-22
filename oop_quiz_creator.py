7e958cd78e389e98d4197b0e66f9c46b2157a9dd
heidilyncaling <calingheidilyn@gmail.com>
2025-05-22 14:20:12 +0000

    setting main screen where the functions will show and where the user will input the data for quiz

--- oop_quiz_creator.py
+++ oop_quiz_creator.py
@@ -1,1 +1,14 @@
-#object oriented programming
+#oop_quizzing_game
+import pygame
+import sys
+import os
+
+#main quiz creator screen 
+pygame.init()
+WIDTH, HEIGHT = 800, 600
+WHITE = (255, 255, 255)
+PURPLE = (255, 0, 255)
+DARK_PURPLE = (100, 0, 150)
+FONT = pygame.font.SysFont("Helvetica", 25)
+BIG_FONT = pygame.font.SysFont("Helvetica", 50)
+
